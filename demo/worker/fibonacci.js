66b1580b284183b2203dfac58794ac81a9db6526
yuanyan <yuanyan.cao@gmail.com>
2012-04-24 22:58:26 +0800

    upadate demo

--- demo/worker/fibonacci.js
+++ demo/worker/fibonacci.js
@@ -1,28 +1,15 @@
-var results = [];
+function fibonacci2(n) {
 
-function resultReceiver(event) {
-    results.push(parseInt(event.data));
-    if (results.length == 2) {
-        self.postMessage(results[0] + results[1]);
+    if(n == 0 || n==1) {
+        return n;
     }
-}
-
-function errorReceiver(event) {
-    throw event.data;
+    else {
+        return(fibonacci2(n - 1) + fibonacci2(n - 2));
+    }
 }
 
 self.onmessage = function(event) {
     var n = parseInt(event.data);
-
-    if (n == 0 || n == 1) {
-        self.postMessage(n);
-        return;
-    }
-
-    for (var i = 1; i <= 2; i++) {
-        var worker = new Worker("fibonacci.js");
-        worker.onmessage = resultReceiver;
-        worker.onerror = errorReceiver;
-        worker.postMessage(n - i);
-    }
-}
+    var res = fibonacci2(n);
+    self.postMessage(res);
+};
